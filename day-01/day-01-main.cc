a2c54b488567b4065d77c7359226aa1d3e2564a5
Gray Houston <grhousto@microsoft.com>
2020-12-11 15:48:01 -0800

    Altered data reader and confirm that it successfully reads in data.

--- day-01/day-01-main.cc
+++ day-01/day-01-main.cc
@@ -5,11 +5,10 @@
 #include "../helper-classes/list-reader.h"
 
 int main(int argc, char** argv) {
-    std::cout << "this is a test";
-    data::ListReaderImpl list_reader("advent-day-01-data.csv");
+    data::ListReaderImpl list_reader("C:/Users/grhousto/advent-of-code/day-01/advent-day-01-data.csv");
 
     for (std::string line : list_reader.GetData()) {
-        std::cout << line;
+        std::cout << line + "\n";
     }
 
     return 0;
--- helper-classes/list-reader-impl.cc
+++ helper-classes/list-reader-impl.cc
@@ -11,10 +11,14 @@ std::vector<std::string> ListReaderImpl::GetData() {
     std::vector<std::string> data_list;
 
     std::ifstream file(this->_filename);
+    if (!file)
+        std::cout << "File couldn't be opened\n";
+
     std::string str;
     while (std::getline(file, str)) {
         data_list.push_back(str);
     }
+    file.close();
 
     return data_list;
 }

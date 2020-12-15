6344edf158e2234849db81b541530e59a606c6de
Gray Houston <grhousto@microsoft.com>
2020-12-14 18:37:57 -0800

    Wrote string parsing for a password entry as a part of the class constructor.

--- day-02/password-entry.cc
+++ day-02/password-entry.cc
@@ -14,5 +14,25 @@ PasswordEntry::PasswordEntry(const std::string line_entry) {
     _low_range = std::stoi(line_entry.substr(lindex, rindex));
     lindex = ++rindex;
 
-    std::cout << "Low Range: " << std::to_string(_low_range) << "\n";
+    //std::cout << "Low Range: " << std::to_string(_low_range) << "\n";
+
+    // Same heuristic assumption as before: no more than 2 digit numbers
+    if (line_entry[++rindex] != ' ') {
+        ++rindex;
+    }
+    _high_range = std::stoi(line_entry.substr(lindex, rindex));
+
+    // std::cout << "High Range: " << std::to_string(_high_range) << "\n";
+
+    // Heuristic assumption: there is only a single character to check for
+    lindex = ++rindex;
+    _required_letter = line_entry[rindex++];
+
+    // std::cout << "Required letter: " << _required_letter << "\n";
+
+    // Heuristic assumption: there is always ": " after every required letter
+    lindex += 3;
+    _password = line_entry.substr(lindex, line_entry.length());
+
+    //std::cout << "Password: '" << _password << "'\n";
 }

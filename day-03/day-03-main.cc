ec30695178b7be7571d5c13f8a63c514ba1fd56a
Gray Houston <grhousto@microsoft.com>
2020-12-15 10:35:15 -0800

    Finished the second half of the day challenge, confirmed on the website.

--- day-03/day-03-main.cc
+++ day-03/day-03-main.cc
@@ -28,23 +28,55 @@ void PrintForestMatrix(const std::vector<std::bitset<row_length>>& matrix) {
 
 } // namespace data
 
+int CalculateTreesHitForSlope(const std::vector<std::bitset<row_length>>& matrix,
+        bool skip_row, int x_increment = 1) {
+    int count = 0;
+
+    for (int y = 0, x = 0; y < matrix.size(); ++y, x = (x + x_increment) % row_length) {
+        if (skip_row && (y % 2 == 1)) {
+            x -= x_increment;
+            continue;
+        }
+
+        int x_compliment = row_length - x - 1;
+        count += matrix[y][x_compliment];
+    }
+
+    return count;
+}
+
 int main(int argc, char** argv) {
     data::ListReaderImpl list_reader("C:/Users/grhousto/advent-of-code/day-03/advent-day-03-data.txt");
     auto tree_matrix = data::ParseForestMatrix(list_reader.GetData());
 
     // First half of solution: determine number of trees hit.
-    int i = 0;
-    int count = 0;
-    for (auto bit_row : tree_matrix) {
-        // Need a compliment index as bitsets are RTL
-        int compliment_index = row_length - i - 1;
-        count += bit_row[compliment_index];
+    // int i = 0;
+    // int count = 0;
+    // for (auto bit_row : tree_matrix) {
+    //     // Need a compliment index as bitsets are RTL
+    //     int compliment_index = row_length - i - 1;
+    //     count += bit_row[compliment_index];
 
-        std::string str = bit_row.to_string('.', '#');
-        str.replace(i, 1, bit_row[compliment_index] ? "X" : "O");
-        std::cout << str << "    count: " << std::to_string(count) << std::endl;
+    //     std::string str = bit_row.to_string('.', '#');
+    //     str.replace(i, 1, bit_row[compliment_index] ? "X" : "O");
+    //     std::cout << str << "    count: " << std::to_string(count) << std::endl;
 
-        i = (i + 3) % row_length;
-    }
-    std::cout << "Number of trees hit: " << std::to_string(count) << std::endl;
+    //     i = (i + 3) % row_length;
+    // }
+    // std::cout << "Number of trees hit: " << std::to_string(count) << std::endl;
+
+    int one_by_one = CalculateTreesHitForSlope(tree_matrix, false, 1);
+    std::cout << "Total hit by 1-1: " << std::to_string(one_by_one) << std::endl;
+
+    int one_by_three = CalculateTreesHitForSlope(tree_matrix, false, 3);
+    std::cout << "Total hit by 1-3: " << std::to_string(one_by_three) << std::endl;
+
+    int one_by_five = CalculateTreesHitForSlope(tree_matrix, false, 5);
+    std::cout << "Total hit by 1-5: " << std::to_string(one_by_five) << std::endl;
+
+    int one_by_seven = CalculateTreesHitForSlope(tree_matrix, false, 7);
+    std::cout << "Total hit by 1-7: " << std::to_string(one_by_seven) << std::endl;
+
+    int two_by_one = CalculateTreesHitForSlope(tree_matrix, true);
+    std::cout << "Total hit by 2-1 : " << std::to_string(two_by_one) << std::endl;
 }

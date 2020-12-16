b9800910ee71fc17d9b9c8ee5b23986fce45faa9
Gray Houston <grhousto@microsoft.com>
2020-12-15 17:50:28 -0800

    Began work on day-04. Starting implementation of string split method

---
+++ helper-classes/string-manipulation.cc
@@ -0,0 +1,14 @@
+// Advent of Code
+
+#include "string-manipulation.h"
+
+namespace data {
+
+std::vector<std::string> string_split(const std::string &str,
+                                      char delim = '\n') {
+  std::vector<std::string> ret;
+
+  return ret;
+}
+
+}
---
+++ helper-classes/string-manipulation.h
@@ -0,0 +1,15 @@
+// Advent of Code
+
+#ifndef HELPER_CLASSES_STRING_MANIPULATION_H_
+#define HELPER_CLASSES_STRING_MANIPULATION_H_
+
+#include <string>
+#include <vector>
+
+namespace data {
+
+std::vector<std::string> string_split(const std::string &str, char delim);
+
+} // namespace data
+
+#endif // HELPER_CLASSES_STRING_MANIPULATION_H_

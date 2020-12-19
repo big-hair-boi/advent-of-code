bd5201e3893a68d641fc459547952358e5058f10
Gray Houston <grhousto@microsoft.com>
2020-12-18 19:47:49 -0800

    Finished day 6 part 2.

--- day-06/day-06-main.cc
+++ day-06/day-06-main.cc
@@ -15,10 +15,16 @@ constexpr const CustomForm EMPTY_FORM = {false, false, false, false, false,
                                          false, false, false, false, false,
                                          false, false, false, false, false,
                                          false};
+constexpr const CustomForm FULL_FORM = {true, true, true, true, true,
+                                         true, true, true, true, true,
+                                         true, true, true, true, true,
+                                         true, true, true, true, true,
+                                         true, true, true, true, true,
+                                         true};
 
 namespace {
 
-CustomForm CreateCustomFormObject(const std::vector<std::string> &str_list) {
+CustomForm CreateCustomFormObjectPart1(const std::vector<std::string> &str_list) {
   CustomForm ret = EMPTY_FORM;
   for (const std::string &str : str_list) {
     for (const char &c : str) {
@@ -28,13 +34,47 @@ CustomForm CreateCustomFormObject(const std::vector<std::string> &str_list) {
   return ret;
 }
 
+CustomForm CreateCustomFormObjectPart2(const std::vector<std::string> &str_list) {
+  std::vector<CustomForm> form_list;
+  for (const std::string &str : str_list) {
+    CustomForm form = EMPTY_FORM;
+    for (const char &c : str) {
+      form[c - 'a'] = true;
+    }
+    form_list.push_back(form);
+  }
+
+  CustomForm composite_form = EMPTY_FORM;
+  for (int i = 0; i < composite_form.size(); ++i) {
+    bool value = true;
+    for (int j = 0; j < form_list.size(); ++j) {
+      value = value && form_list[j][i];
+    }
+    composite_form[i] = value;
+  }
+  return composite_form;
+
+  // Maybe figure out this method for single pass. However i'm tired
+  // CustomForm ret = FULL_FORM;
+  // char prior = 'a';
+  // for (const auto& str : str_list) {
+  //   for (const char& c : str) {
+  //     for (int i = prior - 'a'; i < (c - 'a'); ++i) {
+  //       ret[i] = false;
+  //     }
+  //     prior = c;
+  //   }
+  // }
+  // return ret;
+}
+
 std::vector<CustomForm> BuildCustomFormList() {
   std::vector<CustomForm> ret;
   std::vector<std::string> str_list;
   for (const std::string &line : data::GetStringList(filepath)) {
     // This line is a demarcation between customs forms.
     if (std::all_of(line.begin(), line.end(), isspace)) {
-      ret.push_back(CreateCustomFormObject(str_list));
+      ret.push_back(CreateCustomFormObjectPart2(str_list));
       str_list.clear();
     } else {
       str_list.push_back(line);
@@ -43,7 +83,7 @@ std::vector<CustomForm> BuildCustomFormList() {
 
   // Handle case of final entry being read in but not converted to an object
   if (!str_list.empty()) {
-    ret.push_back(CreateCustomFormObject(str_list));
+    ret.push_back(CreateCustomFormObjectPart2(str_list));
   }
 
   return ret;

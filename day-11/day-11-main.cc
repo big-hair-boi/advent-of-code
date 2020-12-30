3da289f1353d35815a7c7867f8c24c59fabd4d32
Gray Houston <grhousto@microsoft.com>
2020-12-30 14:55:18 -0800

    Finished day 11 part 2.

--- day-11/day-11-main.cc
+++ day-11/day-11-main.cc
@@ -12,7 +12,7 @@ constexpr int row_length = 98;
 
 template <typename T> using SeatMatrix = std::vector<std::array<T, row_length>>;
 
-SeatMatrix<int> CalculateAdjacentSeats(const SeatMatrix<char> &seats) {
+SeatMatrix<int> CalculateAdjacentSeatsPart1(const SeatMatrix<char> &seats) {
   SeatMatrix<int> ret;
 
   for (int i = 0; i < seats.size(); ++i) {
@@ -43,6 +43,53 @@ SeatMatrix<int> CalculateAdjacentSeats(const SeatMatrix<char> &seats) {
   return ret;
 }
 
+SeatMatrix<int> CalculateAdjacentSeatsPart2(const SeatMatrix<char> &seats) {
+  SeatMatrix<int> ret;
+
+  for (int i = 0; i < seats.size(); ++i) {
+    std::array<int, row_length> occupied_seats{0};
+
+    for (int j = 0; j < row_length; ++j) {
+      // only need to calculate adjacency for actual seats
+      if (seats[i][j] == '.') {
+        continue;
+      }
+
+      // Iterate over 3x3 square to calculate number of adjacent seats
+      int num_adjacent = 0;
+      for (int y_dir = i - 1; y_dir <= i + 1; ++y_dir) {
+        for (int x_dir = j - 1; x_dir <= j + 1; ++x_dir) {
+          if (y_dir - i == 0 && x_dir - j == 0) {
+            continue;
+          }
+
+          int row = y_dir;
+          int column = x_dir;
+          while (true) {
+            if (row < 0 || row >= seats.size() || column < 0 ||
+                column >= row_length) {
+              break;
+            }
+
+            if (seats[row][column] == 'L' || seats[row][column] == '#') {
+              num_adjacent += seats[row][column] == '#' ? 1 : 0;
+              break;
+            } else {
+              row += (y_dir - i);
+              column += (x_dir - j);
+            }
+          }
+        }
+      }
+      occupied_seats[j] = num_adjacent;
+    }
+
+    ret.push_back(occupied_seats);
+  }
+
+  return ret;
+}
+
 template <typename T>
 std::ostream &operator<<(std::ostream &os, const SeatMatrix<T> &seats) {
   for (int i = 0; i < seats.size(); ++i) {
@@ -61,26 +108,22 @@ SeatMatrix<char> ReadSeatFormationFromFile() {
 
   for (const auto &line : data::GetStringList(filepath)) {
     std::array<char, row_length> row;
-    std::cout << "Size of line entry: " << line.length() << std::endl
-              << std::flush;
     for (int i = 0; i < row_length; ++i) {
       row[i] = line[i];
     }
     ret.push_back(row);
   }
-
-  std::cout << "Finished reading in data" << std::endl << std::flush;
   return ret;
 }
 
-int CalculateNumEmptySeats(SeatMatrix<char> seats) {
+int CalculateNumEmptySeatsPart1(SeatMatrix<char> seats) {
   // Explicitly take a copy in the argument so that the original is not changed
 
   int num_changes = 1;
   while (num_changes > 0) {
     num_changes = 0;
 
-    auto adjacency_data = CalculateAdjacentSeats(seats);
+    auto adjacency_data = CalculateAdjacentSeatsPart1(seats);
     for (int i = 0; i < seats.size(); ++i) {
       for (int j = 0; j < row_length; ++j) {
         if (seats[i][j] == '.') {
@@ -110,9 +153,48 @@ int CalculateNumEmptySeats(SeatMatrix<char> seats) {
   return ret;
 }
 
+int CalculateNumEmptySeatsPart2(SeatMatrix<char> seats) {
+  // Explicitly take a copy in the argument so that the original is not changed
+
+  int num_changes = 1;
+  while (num_changes > 0) {
+    num_changes = 0;
+
+    auto adjacency_data = CalculateAdjacentSeatsPart2(seats);
+    for (int i = 0; i < seats.size(); ++i) {
+      for (int j = 0; j < row_length; ++j) {
+        if (seats[i][j] == '.') {
+          continue;
+        }
+
+        if (seats[i][j] == '#' && adjacency_data[i][j] >= 5) {
+          ++num_changes;
+          seats[i][j] = 'L';
+        } else if (seats[i][j] == 'L' && adjacency_data[i][j] == 0) {
+          ++num_changes;
+          seats[i][j] = '#';
+        }
+      }
+    }
+  }
+
+  int ret = 0;
+  for (int i = 0; i < seats.size(); ++i) {
+    for (int j = 0; j < row_length; ++j) {
+      if (seats[i][j] == '#') {
+        ++ret;
+      }
+    }
+  }
+
+  return ret;
+}
+
 int main(int argc, char **argv) {
   auto seat_data = ReadSeatFormationFromFile();
 
-  std::cout << "Part 1 - Final number or taken seats: "
-            << CalculateNumEmptySeats(seat_data);
+  std::cout << "Part 1 - Final number of taken seats: "
+            << CalculateNumEmptySeatsPart1(seat_data) << std::endl;
+  std::cout << "Part 2 - Final number of taken seats: "
+            << CalculateNumEmptySeatsPart2(seat_data) << std::endl;
 }
